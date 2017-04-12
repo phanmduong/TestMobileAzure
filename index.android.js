dc584ae15a69e9347d14c164cd20bdd6b6eab628
phanmduong <phanminhduong@hotmail.com>
2017-04-12 18:38:43 +0700

    code push android

--- index.android.js
+++ index.android.js
@@ -6,48 +6,55 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
+import codePush from "react-native-code-push";
+
+let codePushOptions = {updateDialog: true, installMode: codePush.InstallMode.IMMEDIATE};
 
 export default class TestMobileAzure extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    render() {
+        return (
+            <View style={styles.container}>
+              <Text style={styles.welcome}>
+                Phan M Duong Developer
+              </Text>
+              <Text style={styles.instructions}>
+                To get started, edit index.ios.js
+              </Text>
+              <Text style={styles.instructions}>
+                Press Cmd+R to reload,{'\n'}
+                Cmd+D or shake for dev menu
+              </Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
+
+
+TestMobileAzure = codePush(codePushOptions)(TestMobileAzure);
 
 AppRegistry.registerComponent('TestMobileAzure', () => TestMobileAzure);
